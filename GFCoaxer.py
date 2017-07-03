c9f6bf8692d82020a968abde87576bdffc3c6560
ss <2bsunshine4u@gmail.com>
2017-07-03 20:28:10 +0800

    add force on

--- GFCoaxer.py
+++ GFCoaxer.py
@@ -12,7 +12,7 @@ class GFCoaxerBot(WXBot):
 
         self.tuling_key = ""
         self.robot_switch = True
-        self.robotOn = False
+        self.robotOn = 0 #0:off 1:auto on 2: force on
         self.lastSpeak = None
         self.GFid = None
 
@@ -53,24 +53,38 @@ class GFCoaxerBot(WXBot):
     def handle_msg_all(self, msg):
         #if GF send me something
         if msg['msg_type_id'] == 4 and msg['content']['type'] == 0 and msg['user']['id'] == self.GFid:
-            #if robot is not on and timer is None, start the timer
-            if not self.robotOn:
-                if self.lastSpeak == None:
+            #if robot is not on
+            if self.robotOn == 0:
+                #if the content is calling its name, force on
+                if msg['content']['data'] == u'邵小宝':
+                    self.robotOn = 2
+                    result = "我邵小宝来也~"
+                    self.send_msg_by_uid(result, msg['user']['id'])
+                    print '    ROBOT:', result
+                    self.lastSpeak = None
+                #if timer is None, start the timer and record the userID
+                elif self.lastSpeak == None:
                     #print "Timer starts!"
-                    self.lastSpeak = time.time()
-                    return
+                    self.lastSpeak = (time.time(), msg['user']['id'])
             #if robot is on, send auto reply
             else:
                 reply = self.tuling_auto_reply(msg['user']['id'], msg['content']['data'])
                 self.send_msg_by_uid(reply, msg['user']['id'])
-        #if I send GF anything, turn off the robot and reset the timer
+        #if I send GF anything
         elif msg['msg_type_id'] == 1 and msg['user']['name'] == 'self' and msg['to_user_id'] == self.GFid:
-            #print "I spoke and reset the timer!"
-            if self.robotOn:
-                self.robotOn = False
+            #turn off the robot if it's auto on and reset the timer
+            if self.robotOn == 1:
+                self.robotOn = 0
                 result = u'邵大帅来啦！我邵小宝先撤啦！'
-                self.send_msg_by_uid(result, self.GFid)
+                self.send_msg_by_uid(result, msg['to_user_id'])
                 print '    ROBOT:', result
+            #if forced on and I told it to quit, it will quit
+            elif self.robotOn == 2:
+                if msg['content']['data'] == u'小宝去写作业':
+                    self.robotOn = 0
+                    result = u'邵大帅赶我走啦~拜拜啦'
+                    self.send_msg_by_uid(result, msg['to_user_id'])
+                    print '    ROBOT:', result
             self.lastSpeak = None
         #if group chat
         elif msg['msg_type_id'] == 3:
@@ -79,16 +93,16 @@ class GFCoaxerBot(WXBot):
     #every 5 seconds, check if the time since last speak has been over 1 min
     def schedule(self):
         curTime = time.time()
-        if not self.robotOn and not self.lastSpeak == None and curTime - self.lastSpeak >= 60:
-            self.robotOn = True
-            result = u'邵大帅在忙呢，先让我邵小宝来陪你聊聊呗~'
-            self.send_msg_by_uid(result, self.GFid)
+        if not self.robotOn and not self.lastSpeak == None and curTime - self.lastSpeak[0] >= 60:
+            self.robotOn = 1
+            result = u'邵大帅在忙呢，先让我邵小宝来陪你聊聊呗~你刚说了啥再说一遍咯'
+            self.send_msg_by_uid(result, self.lastSpeak[1])
             print '    ROBOT:', result
         time.sleep(5)
-    #overwrite get_contact to initiate GF's id
+    #overwrite get_contact to initialize GF's id
     def get_contact(self):
         if WXBot.get_contact(self):
-            #initiate GF's userID
+            #initialize GF's userID
             self.GFid = self.get_user_id(u'老婆')
             if not self.GFid:
                 sys.exit('GF account not found!')
